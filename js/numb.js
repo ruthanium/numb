2e177c9e00f042753f3eb3f27efe951ae1abf163
ruthanium <ruthanium@gmail.com>
2015-06-23 23:49:52 -0400

    Mockup complete
    basic structure built.

---
+++ js/numb.js
@@ -0,0 +1,13 @@
+$().ready(function() {
+
+  $("#eqCover").css('cursor', 'pointer').click(startQuestions());
+  $("#timestats").hide();
+
+});
+
+// launches the first question.
+function startQuestions()
+{
+  //$("#timestats").show();
+  console.log("starting questions....");
+}
